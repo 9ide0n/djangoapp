4218a7e8079f754534b3df5d281589cd4fbe38d8
gide0n <->
2017-12-06 03:12:51 +0300

    Add testapp to perform cpu app load and db load (cpu+disk)

--- djangopg/urls.py
+++ djangopg/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls',namespace="polls")),
+    url(r'^tests/', include('tests.urls',namespace="tests")),
     url(r'^admin/', admin.site.urls),
 ]
---
+++ tests/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include,url
+
+from . import views
+
+urlpatterns = [
+    url(r'app/$', views.app, name='app'),
+    # url(r't1/$', views.t1, name='t1'),
+    # url(r't2/$', views.t2, name='t2'),
+    # url(r't3/$', views.t3, name='t3'),
+    url(r'db/(?P<table_id>(1|2|3){1})$', views.db, name='db'),
+
+]
